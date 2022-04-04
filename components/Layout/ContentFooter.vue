9ac8e268a621cf792c268b3f454449c28c2d8b4b
Nicolas Dirazar <jndirazar@gmail.com>
2022-04-04 15:22:59 -0300

    Iniciando Reposistorio

---
+++ components/Layout/ContentFooter.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a
+            href="https://ioticos.org"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            IoTicos.org
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://ioticos.org"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            About Us
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="hhttps://ioticos.org"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+      <div class="copyright">
+        &copy; Copyright {{ year }} por
+
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener"
+          >Nicolas Dirazar</a
+        >
+        for a better web.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
